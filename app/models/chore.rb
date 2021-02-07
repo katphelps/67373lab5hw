098d646231898f972254db7b7d1aaad711fd50dc
Kat Phelps <kphelps2@andrew.cmu.edu>
2021-02-07 17:54:28 -0500

    working but only w the chore stuff

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,2 +1,20 @@
 class Chore < ApplicationRecord
+    belongs_to :child
+    belongs_to :task
+
+    validates_date :due_on
+
+    scope :by_task, -> { order('task') }
+    scope :chronological, -> {order('due_on')}
+    scope :pending, -> { where(completed: false) }
+    scope :done, ->  { where(completed: true) }
+    scope :upcoming, ->  { where('due_on > ?', Date.today) }
+    scope :past, ->  { where('due_on > ?', Date.today) }
+
+    def status
+        if self.completed == true
+            'Completed'
+        end
+        'Pending'
+    end
 end
